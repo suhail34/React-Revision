b040cdb6ed1b6049897578e21be6e3b8322194a7
suhail34 <suhailkna@gmail.com>
2023-11-09 21:57:24 +0530

    UseEffect and UseState hook
    Signed-off-by: suhail34 <suhailkna@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if(input!==""){
+        const resp = await fetch(`https://jsonplaceholder.typicode.com/users/${input}/todos`)
+        const data = await resp.json();
+        setTodos(data);
+        console.log(data)
+      }else {
+        console.log("input is empty")
+        setTodos([]);
+      }
+    }
+    fetchData()
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User and there todos</h1>
+      <input type="text" value={input} onChange={(e)=>{
+        setInput(e.target.value)
+      }} placeholder='Enter User ID' />
+      <ul>
+        {Array.isArray(todos) && todos.length > 0 ? todos.map((todo, index)=>(
+          <li key={index}>
+            {!todo.completed?
+              <h3>{todo.title}</h3>:
+              <h3 style={{textDecoration:"line-through"}}>{todo.title}</h3>
+            }
+          </li>
+        )):<p>No user selected to view the todos</p>}
+      </ul>
     </div>
   );
 }
